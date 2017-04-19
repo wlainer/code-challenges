f7d9b6bd926f1f8adab966ecfd1b78a70d5c7841
Wlainer Ribeiro <wlainer@gmail.com>
2017-04-18 22:02:48 -0300

    updating

--- src/main/java/codility/prefixsums/CountDivs.java
+++ src/main/java/codility/prefixsums/CountDivs.java
@@ -5,20 +5,36 @@ import org.junit.Test;
 
 public class CountDivs {
 	
-	public int solution(int A, int B, int K) {
-		if (A % K == 0)
-			return B/K - A/K +1;
-
-		return B/K - A/K;
+	public int solution(int[] A) {
+		
+		int max = Integer.MIN_VALUE;
+		int[] triplet = new int[3];
+		int count = 0;
+		for (int i = 0; i < A.length - 2; i++) {
+			triplet[count++] = A[i];
+			for (int j = i + 1; j < A.length; j++) {
+				if (A[j]>= triplet[count -1]) triplet[count++] = A[j];
+				
+				if (count == 3) break;
+			}
+			
+			if (count == 3 && triplet[0]*triplet[1]*triplet[2] > max) max = triplet[0]*triplet[1]*triplet[2];
+			count = 0;
+		}
+		
+		return max;
 	}
 	
 	@Test
 	public void test() {
-		int A = 6;
-		int B = 11;
-		int K = 2;
+		int[] A = new int[] {-3, 1, 2, -2, 5, 6};
+		Assert.assertEquals(solution(A), 60);
 		
-		Assert.assertEquals(solution(A, B, K), 3);
+		A = new int[] {10,10,10};
+		Assert.assertEquals(solution(A), 1000);
+		
+		A = new int[] {4, 5, 1, 0};
+		Assert.assertEquals(solution(A), 20);
 		
 	}
 
---
+++ src/main/java/codility/sorting/MaxProductOfThree.java
@@ -0,0 +1,34 @@
+package codility.sorting;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaxProductOfThree {
+
+	public int solution(int[] A) {
+		Arrays.sort(A);
+		
+		int distinct = 0;
+		for (int i = 1; i < A.length-1; i++) 
+			if (A[i] == A[i-1]) distinct++;
+
+		return A.length - distinct;
+		
+	}
+
+	@Test
+	public void test() {
+		int[] A = new int[] { 2, 1, 1, 2, 3, 1 };
+		Assert.assertEquals(solution(A), 3);
+		
+		A = new int[] {1,1,1,1,1,1};
+		Assert.assertEquals(solution(A), 1);
+		
+		A = new int[] {};
+		Assert.assertEquals(solution(A), 0);
+
+	}
+
+}
