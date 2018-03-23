57f9466de7683694b862e78ef00bb7a70b817413
Wlainer Ribeiro <wlainer@gmail.com>
2018-03-22 23:13:48 -0300

    updating

--- src/main/java/challenge/hackerearth/recursion/Snake.java
+++ src/main/java/challenge/hackerearth/recursion/Snake.java
@@ -1,6 +1,7 @@
 package challenge.hackerearth.recursion;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,143 +14,189 @@ import challenge.TestBase;
 
 public class Snake extends TestBase {
 
-	private static final String direction = "rdlu";
-	private static Integer snakeSize = 7;
-	private static Integer boardSize = 10;
+	private static final String DIRECTION = "rdlu";
+	private static Integer SNAKE_SIZE = 7;
+	private static boolean FIND_FIRST = true;
+	private Integer boardSize;
 
 	public void solution() {
 
 		List<String> lines = null;
-		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("./challenge/hackerearth/recursion/Snake_01_in.txt")));
-
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getClassLoader()
+				.getResourceAsStream("./challenge/hackerearth/recursion/Snake_01_in.txt")))) {
 			lines = new ArrayList<String>();
 			String strLine;
-			// Read File Line By Line
 			while ((strLine = reader.readLine()) != null) {
-				// Print the content on the console
 				lines.add(strLine);
 			}
-
-			// Close the input stream
-			reader.close();
-		} catch (Exception e) {
-
+		} catch (IOException e) {
+			System.err.println("Error while reading the file.");
+			e.printStackTrace();
 		}
 
-		int size = lines.size();
+		boardSize = lines.size();
 
-		int[][] grid = new int[size][size];
+		int[][] grid = new int[boardSize][boardSize];
 		for (int i = 0; i < grid.length; i++) {
-			String string = lines.get(i);
-			String[] split = string.split(",");
+			String line = lines.get(i);
+			String[] values = line.split(",");
 
-			for (int j = 0; j < split.length; j++) {
-				grid[i][j] = Integer.valueOf(split[j].trim());
+			for (int j = 0; j < values.length; j++)
+				grid[i][j] = Integer.valueOf(values[j].trim());
+		}
 
+		List<String> pathways = generateCombinations(SNAKE_SIZE - 1, DIRECTION.toCharArray());
+
+		Map<Integer, List<Structure>> map = new HashMap();
+
+		for (int i = 0; i < boardSize; i++) {
+			for (int j = 0; j < boardSize; j++) {
+				for (String path : pathways) {
+					Integer sum = getSum(i, j, grid, path);
+					if (sum != null) {
+						Structure structure = new Structure(path, i, j);
+						List<Structure> list;
+						if (map.get(sum) == null) {
+							list = new ArrayList<>();
+							list.add(structure);
+							map.put(sum, list);
+						} else {
+							list = map.get(sum);
+							if (!verifyStructure(structure, list)) {
+								list.add(structure);
+								map.put(sum, list);
+
+								printSnakes(list);
+								if (FIND_FIRST)
+									System.exit(0);
+
+							}
+						}
+					}
+				}
 			}
+		}
+		
+//		int count = 0;
+//		for (Entry<Integer, List<Structure>> e: map.entrySet()) {
+//			if (e.getValue().size() > 1) {
+//				count++;
+//				if (count == 500) {
+//					printSnakes(e.getValue());
+//				}
+//			}
+//		}
+//		
+//		System.out.println(count);
+	}
+
+	private void printSnakes(List<Structure> list) {
+		for (Structure structure : list) {
+			List<Coordinates> coordinates = getCoordinates(structure.getRow(), structure.getCol(), structure.getPath());
+			plotCoord(coordinates);
+		}
+	}
+
+	private Integer getSum(int row, int col, int[][] grid, String path) {
+		List<Coordinates> coordinates = getCoordinates(row, col, path);
+		int[][] auxiliarGrid = new int[boardSize][boardSize];
+		initializeBoard(auxiliarGrid);
+
+		Integer sum = 0;
+		for (Coordinates coord : coordinates) {
+			if ((coord.getRow() >= 0 && coord.getRow() < boardSize)
+					&& (coord.getCol() >= 0 && coord.getCol() < boardSize)
+					&& auxiliarGrid[coord.getRow()][coord.getCol()] != 1) {
+				auxiliarGrid[coord.getRow()][coord.getCol()] = 1;
+				sum += grid[coord.getRow()][coord.getCol()];
+			} else
+				return null;
+		}
+
+		if (checkSurroundings(coordinates, auxiliarGrid))
+			return sum;
+		return null;
+	}
+
+	private boolean checkSurroundings(List<Coordinates> coordinates, int[][] auxiliarGrid) {
+
+		for (Coordinates coord : coordinates) {
+			Integer count = 0;
+			// right
+			if (coord.getCol() + 1 < boardSize && auxiliarGrid[coord.getRow()][coord.getCol() + 1] == 1)
+				count++;
+
+			// left
+			if (coord.getCol() - 1 >= 0 && auxiliarGrid[coord.getRow()][coord.getCol() - 1] == 1)
+				count++;
+
+			// up
+			if (coord.getRow() - 1 >= 0 && auxiliarGrid[coord.getRow() - 1][coord.getCol()] == 1)
+				count++;
+
+			// down
+			if (coord.getRow() + 1 < boardSize && auxiliarGrid[coord.getRow() + 1][coord.getCol()] == 1)
+				count++;
+
+			if (count > 2)
+				return false;
 
 		}
 
-		List<String> a = new ArrayList();
-		a.add("r");
-		a.add("l");
-		a.add("u");
-		a.add("d");
+		return true;
 
-		List<String> generateCombinations = generateCombinations(snakeSize - 1, (ArrayList<String>) a);
-		//
-		// for (String string : generateCombinations) {
-		// System.out.println(string);
-		// }
+	}
 
-//		int[][] board = new int[size][size];
+	private List<Coordinates> getCoordinates(int row, int col, String path) {
+		List<Coordinates> list = new ArrayList<>();
+		Coordinates coord = new Coordinates(row, col);
+		list.add(coord);
+		for (char c : path.toCharArray()) {
+			switch (c) {
+			case 'd':
+				row++;
+				break;
+			case 'u':
+				row--;
+				break;
+			case 'l':
+				col--;
+				break;
+			case 'r':
+				col++;
+				break;
+			default:
+				break;
+			}
+			coord = new Coordinates(row, col);
+			list.add(coord);
+		}
+		return list;
+	}
+
+	private boolean verifyStructure(Structure st1, List<Structure> list) {
+		List<Coordinates> coordinatesSt1 = getCoordinates(st1.getRow(), st1.getCol(), st1.getPath());
+
+		for (Structure structure : list) {
+			List<Coordinates> coordinatesSt2 = getCoordinates(structure.getRow(), structure.getCol(),
+					structure.getPath());
+
+			for (Coordinates coordinates : coordinatesSt2) {
+				if (coordinatesSt1.contains(coordinates))
+					return true;
+			}
+		}
+		return false;
+	}
+
+	private void plotCoord(List<Coordinates> coordinates) {
 		int[][] board = new int[boardSize][boardSize];
-		int superCount = 0;
-		Map<Integer, Integer> map = new HashMap();
-		outer: for (String string : generateCombinations) {
-			initializeBoard(board);
-			int row = 0;
-			int col = 0;
-			board[row][col] = 1;
+		initializeBoard(board);
 
-			int sum = grid[row][col];
-			int cont = 0;
-			inner: for (char c : string.toCharArray()) {
-				switch (c) {
-				case 'r':
-					col = col + 1;
+		for (Coordinates coord : coordinates)
+			board[coord.getRow()][coord.getCol()] = 1;
 
-					if (col >= 0 && col < 4 && board[row][col] != 1) {
-						sum += grid[row][col];
-						board[row][col] = 1;
-						cont++;
-					} else
-						continue outer;
-					break;
-				case 'l':
-					col = col - 1;
-
-					if (col >= 0 && col < 4 && board[row][col] != 1) {
-						sum += grid[row][col];
-						board[row][col] = 1;
-						cont++;
-					} else
-						continue outer;
-					break;
-				case 'u':
-					row = row - 1;
-
-					if (row >= 0 && row < 4 && board[row][col] != 1) {
-						sum += grid[row][col];
-						board[row][col] = 1;
-						cont++;
-					} else
-						continue outer;
-					break;
-				case 'd':
-					row = row + 1;
-
-					if (row >= 0 && row < 4 && board[row][col] != 1) {
-						sum += grid[row][col];
-						board[row][col] = 1;
-						cont++;
-					} else
-						continue outer;
-					break;
-
-				default:
-					break;
-				}
-
-			}
-			if (cont == snakeSize - 1) {
-				if (map.get(sum) == null) {
-					map.put(sum, 0);
-				} else {
-					map.put(sum, map.get(sum) + 1);
-				}
-				
-				superCount++;
-				System.out.println("-------------");
-				printBoard(board);
-			}
-		}
-
-		System.out.println(superCount);
-
-		// Scanner s = getScanner();
-		//
-		// int boardSize = s.nextInt();
-		//
-		// int[][] board = new int[boardSize][boardSize];
-		// for (int i = 0; i < boardSize; i++)
-		// for (int j = 0; j < boardSize; j++)
-		// board[i][j] = 0;
-		//
-		// boolean result = move(board, 0, 0, 0);
-		// printBoard(4, board);
-
+		printBoard(board);
 	}
 
 	private void initializeBoard(int[][] board) {
@@ -165,121 +212,11 @@ public class Snake extends TestBase {
 
 			System.out.println();
 		}
+		System.out.println("#####################");
+		System.out.println();
 	}
 
-	private boolean move(int[][] board, int x, int y, int direction) {
-		// if (snakeSize > 0)
-		// canMove(x, y, board)
-
-		printBoard(board);
-		System.out.println("---------------");
-
-		board[x][y] = board[x][y] + 1;
-		snakeSize--;
-
-		char d = this.direction.charAt(direction % 4);
-		if (snakeSize > 0) {
-			switch (d) {
-			case 'r':
-				int row = x;
-				int col = y + 1;
-				if (row >= 0 && col < board.length)
-					move(board, row, col, 0);
-
-				break;
-			case 'l':
-				row = x;
-				col = y - 1;
-				if (row >= 0 && col < board.length)
-					move(board, row, col, 2);
-
-				break;
-			case 'u':
-				row = x - 1;
-				col = y;
-				if (row >= 0 && col < board.length)
-					move(board, row, col, 3);
-
-				break;
-			case 'd':
-				row = x + 1;
-				col = y;
-				if (row >= 0 && col < board.length)
-					move(board, row, col, 1);
-
-				break;
-
-			default:
-				break;
-			}
-		} else {
-			snakeSize = snakeSize + 2;
-			switch (d) {
-			case 'r':
-				int row = x;
-				int col = y - 1;
-				if (row >= 0 && col < board.length)
-					move(board, row, col, direction + 1);
-
-				break;
-			case 'l':
-				row = x;
-				col = y + 1;
-				if (row >= 0 && col < board.length)
-					move(board, row, col, direction + 1);
-
-				break;
-			case 'u':
-				row = x + 1;
-				col = y;
-				if (row >= 0 && col < board.length)
-					move(board, row, col, direction + 1);
-
-				break;
-			case 'd':
-				row = x - 1;
-				col = y;
-				if (row >= 0 && col < board.length)
-					move(board, row, col, direction + 1);
-
-				break;
-
-			default:
-				break;
-			}
-		}
-
-		return false;
-	}
-
-	@Test
-	public void test() {
-		restoreOldOut();
-
-		solution();
-
-		String[] lines = getLines();
-		// Assert.assertEquals("true", lines[0]);
-	}
-
-	public static void possibleStrings(int maxLength, char[] alphabet, String curr) {
-
-		// If the current string has reached it's maximum length
-		if (curr.length() == maxLength) {
-			System.out.println(curr);
-
-			// Else add each letter from the alphabet to new strings and process these new strings again
-		} else {
-			for (int i = 0; i < alphabet.length; i++) {
-				String oldCurr = curr;
-				curr += alphabet[i];
-				possibleStrings(maxLength, alphabet, curr);
-				curr = oldCurr;
-			}
-		}
-	}
-
-	private List<String> generateCombinations(int arraySize, ArrayList<String> possibleValues) {
+	private List<String> generateCombinations(int arraySize, char[] cs) {
 		StringBuffer buf;
 		List<String> arr = new ArrayList<String>();
 
@@ -288,7 +225,7 @@ public class Snake extends TestBase {
 		do {
 			buf = new StringBuffer();
 			for (int index : indices)
-				buf.append(possibleValues.get(index));
+				buf.append(cs[index]);
 
 			arr.add(buf.toString());
 
@@ -300,7 +237,7 @@ public class Snake extends TestBase {
 				indices[i] += carry;
 				carry = 0;
 
-				if (indices[i] == possibleValues.size()) {
+				if (indices[i] == cs.length) {
 					carry = 1;
 					indices[i] = 0;
 				}
@@ -308,6 +245,87 @@ public class Snake extends TestBase {
 		} while (carry != 1);
 
 		return arr;
+	}
+
+	class Structure {
+
+		private String path;
+		private int row, col;
+
+		public Structure(String path, int row, int col) {
+			super();
+			this.path = path;
+			this.row = row;
+			this.col = col;
+		}
+
+		public String getPath() {
+			return path;
+		}
+
+		public void setPath(String path) {
+			this.path = path;
+		}
+
+		public int getRow() {
+			return row;
+		}
+
+		public void setRow(int row) {
+			this.row = row;
+		}
+
+		public int getCol() {
+			return col;
+		}
+
+		public void setCol(int col) {
+			this.col = col;
+		}
+
+	}
+
+	class Coordinates {
+		private Integer row;
+		private Integer col;
+
+		public Coordinates(Integer row, Integer col) {
+			this.row = row;
+			this.col = col;
+		}
+
+		public Integer getRow() {
+			return row;
+		}
+
+		public void setRow(Integer row) {
+			this.row = row;
+		}
+
+		public Integer getCol() {
+			return col;
+		}
+
+		public void setCol(Integer col) {
+			this.col = col;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			Coordinates coord = (Coordinates) obj;
+			return this.col == coord.getCol() && this.row == coord.getRow();
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%s,%s", this.row, this.col);
+		}
+	}
+
+	@Test
+	public void test() {
+		restoreOldOut();
+		solution();
 	}
 
 }
